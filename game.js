ff3feb19723c0d473550898eea674f338028f0f6
Victor Veytsman <vityavv@gmail.com>
2017-11-10 17:13:20 -0500

    Set up BEAUTIFUL object for the ball

--- game.js
+++ game.js
@@ -3,6 +3,16 @@ function $(element) {
 } //Looks a lot like jQuery, but _it's not_
 
 var canvas, ctx;//These are the variables that depend on DOM, so we initialize them after load.
+var ball = {
+	velocity = {
+		x = Math.random().toFixed(2),
+		y = 1 - x
+	},
+	position = {
+		x = 600,
+		y = 400
+	}
+};//beautiful
 
 function start() {
 	canvas = $("pongcanvas");
@@ -10,6 +20,10 @@ function start() {
 
 	ctx.fillRect(0, 0, 1200, 800);
 }
-
+function update() {
+	ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, 1200, 800);
+	ctx.fillStyle = "white";
+}
 
 window.onload = start;
